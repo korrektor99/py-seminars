40509e1984b4cf6ff435e8c6f68dff742d574181
2 1 <korrektorrussia@mail.ru>
2022-12-18 12:04:11 +0700

    dz 3.4 done

---
+++ dz3.4.py
@@ -0,0 +1,17 @@
+n = int(input('Введите число: '))
+
+def getfib(n):
+    fibo_nums = []
+    a, b = 1, 1
+    for i in range(n-1):
+        fibo_nums.append(a)
+        a, b = b, a + b
+    a, b = 0, 1
+    for i in range (n):
+        fibo_nums.insert(0, a)
+        a, b = b, a - b
+    return fibo_nums
+
+fibo_nums = getfib(n)
+print(getfib(n))
+print(fibo_nums.index(0))
