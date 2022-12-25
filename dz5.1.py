593dbc9657b069d9647de736f30cc27351a360cb
2 1 <korrektorrussia@mail.ru>
2022-12-25 19:39:08 +0700

    dz5.1 done

---
+++ dz5.1.py
@@ -0,0 +1,11 @@
+#Напишите программу, удаляющую из текста все слова, содержащие ""абв"".
+
+
+my_text = 'абвпривет здорово наверное ужасабв какабввсеабвуспеть"'
+
+def del_some_words(my_text):
+    my_text = list(filter(lambda x: 'абв' not in x, my_text.split()))
+    return " ".join(my_text)
+
+my_text = del_some_words(my_text)
+print(my_text)
