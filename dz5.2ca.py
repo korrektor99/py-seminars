531d569cf976213c2ba4c07bb537dcb5350f6363
2 1 <korrektorrussia@mail.ru>
2022-12-25 20:40:47 +0700

    dz5.2 done

---
+++ dz5.2ca.py
@@ -0,0 +1,107 @@
+
+#Создайте программу для игры с конфетами человек против человека.
+
+#Условие задачи: На столе лежит 2021 конфета. Играют два игрока делая ход друг после друга. Первый ход определяется жеребьёвкой. За один ход можно забрать не более чем 28 конфет. Все конфеты оппонента достаются сделавшему последний ход. Сколько конфет нужно взять первому игроку, чтобы забрать все конфеты у своего конкурента?
+
+#a) Добавьте игру против бота
+
+from random import *
+import os
+
+message = ['очередь']
+
+
+def player_vs_player():
+    candies_total = 2021
+    max_take = 28
+    count = 0
+    player_1 = input('\nимя 1 игрока: ')
+    player_2 = input('\nимя 2 игрока : ')
+
+    print(f'\n  {player_1} и  {player_2} вступили в бой !\n')
+    print('\nДля начала опеределим кто первый начнет игру.\n')
+
+    x = randint(1, 2)
+    if x == 1:
+        lucky = player_1
+        loser = player_2
+    else:
+        lucky = player_2
+        loser = player_1
+    print(f' {lucky}начинает!')
+
+    while candies_total > 0:
+        if count == 0:
+            step = int(input(f'\n{choice(message)} {lucky} = '))
+            if step > candies_total or step > max_take:
+                step = int(input(
+                    f'\n сликом много ограничение {max_take} конфет {lucky}, попробуй еще раз: '))
+            candies_total = candies_total - step
+        if candies_total > 0:
+            print(f'\nна столе {candies_total}')
+            count = 1
+        else:
+            print('конец')
+
+        if count == 1:
+            step = int(input(f'\n{choice(message)}, {loser} '))
+            if step > candies_total or step > max_take:
+                step = int(input(
+                    f'\nсликом много ограничение  {max_take} конфет {loser}, попробуй еще раз: '))
+            candies_total = candies_total - step
+        if candies_total > 0:
+            print(f'\nна кону еще {candies_total}')
+            count = 0
+        else:
+            print('кончились конфеты')
+
+    if count == 1:
+        print(f'{loser} ПОБЕДИЛ')
+    if count == 0:
+        print(f'{lucky} ПОБЕДИЛ')
+
+
+player_vs_player()
+
+
+def player_vs_bot():
+    candies_total = 2021
+    max_take = 28
+    player_1 = input('\nКак тебя зовут?: ')
+    player_2 = 'Компьютер'
+    players = [player_1, player_2]
+    print(f'\nНу чтож {player_1} и  {player_2} да начнется игра !\n')
+    print('\nДля начала опеределим кто первый начнет игру.\n')
+
+    lucky = randint(-1, 0)
+
+    print(f'Поздравляю {players[lucky+1]} ты ходишь первым !')
+
+    while candies_total > 0:
+        lucky += 1
+
+        if players[lucky % 2] == 'Компьютер':
+            print(
+                f'\nХодит {players[lucky%2]} \nНа кону {candies_total}. \n{choice(message)}: ')
+
+            if candies_total < 29:
+                step = candies_total
+            else:
+                delenie = candies_total//28
+                step = candies_total - ((delenie*max_take)+1)
+                if step == -1:
+                    step = max_take -1
+                if step == 0:
+                    step = max_take
+            while step > 28 or step < 1:
+                step = randint(1,28)
+            print(step)
+        else:
+            step = int(input(f'\nНу чтож ходи,  {players[lucky%2]} \nНа кону {candies_total} {choice(message)}:  '))
+            while step > max_take or step > candies_total:
+                step = int(input(f'\nЗа один ход можно взять {max_take} конфет, попробуй еще раз: '))
+        candies_total = candies_total - step
+
+    print(f'На кону осталось {candies_total} \nПобедил {players[lucky%2]}')
+
+player_vs_bot()
